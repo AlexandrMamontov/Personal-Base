7fc7c569b8dab305024197caf7623133616acd44
alexandr_mamontov <sasham13101999@yandex.ru>
2025-04-14 15:46:42 +0300

    personal base changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const headerBlocks = document.querySelectorAll('.personal-base__block');
-    headerBlocks.forEach((headerBlock) => {
-        const headerBtn = headerBlock.querySelector('.personal-base__btn');
-        headerBtn.addEventListener('click', function() {
-            const value = headerBlock.dataset.column;
-            const cells = document.querySelectorAll(`.personal-base__cell[data-column="${value}"]`);
-            headerBtn.classList.toggle('active');
-            headerBlock.classList.toggle('active');
-            cells.forEach((cell) => {
-                cell.classList.toggle('active');
-            });
-        })
-    })
-})
+// document.addEventListener('DOMContentLoaded', function() {
+//     const headerBlocks = document.querySelectorAll('.personal-base__block');
+//     headerBlocks.forEach((headerBlock) => {
+//         const headerBtn = headerBlock.querySelector('.personal-base__btn');
+//         headerBtn.addEventListener('click', function() {
+//             const value = headerBlock.dataset.column;
+//             const cells = document.querySelectorAll(`.personal-base__cell[data-column="${value}"]`);
+//             headerBtn.classList.toggle('active');
+//             headerBlock.classList.toggle('active');
+//             cells.forEach((cell) => {
+//                 cell.classList.toggle('active');
+//             });
+//         })
+//     })
+// })
