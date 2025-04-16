edebaa3a2f48403347200eb20f4570f56ebb7dec
alexandr_mamontov <sasham13101999@yandex.ru>
2025-04-16 22:19:59 +0300

    personal base changes

--- js/script.js
+++ js/script.js
@@ -17,14 +17,34 @@ document.addEventListener('DOMContentLoaded', function() {
     // радиокнопки
     const radioButtons = document.querySelectorAll('.personal-base__radio');
     const table = document.querySelector('.personal-base__table');
+    const headerBtns = document.querySelectorAll('.personal-base__btn');
+    const cells2 = document.querySelectorAll('.personal-base__cell');
     radioButtons.forEach((radio) => {
         radio.addEventListener('change', function() {
             if (this.id === 'all-status') {
                 table.classList.remove('work-status');
                 table.classList.add('all-status');
+                headerBlocks.forEach((headerBlock) => {
+                    headerBlock.classList.remove('active');
+                });
+                cells2.forEach((cell) => {
+                    cell.classList.remove('active');
+                });
+                headerBtns.forEach((headerBtn) => {
+                    headerBtn.classList.remove('active');
+                });
             } else if (this.id === 'work-status') {
                 table.classList.remove('all-status');
                 table.classList.add('work-status');
+                headerBlocks.forEach((headerBlock) => {
+                    headerBlock.classList.add('active');
+                });
+                cells2.forEach((cell) => {
+                    cell.classList.add('active');
+                });
+                headerBtns.forEach((headerBtn) => {
+                    headerBtn.classList.add('active');
+                });
             }
         })
     })
